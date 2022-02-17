0fb66f686e5356e7aeac88b4cabc820d3bca46fe
Troy Boone <troy.boone@autodesk.com>
2022-02-17 14:42:39 -0500

    plz

--- test_pii_data.py
+++ test_pii_data.py
@@ -6,7 +6,7 @@ from pii_data import Pii
 class DataTestCases(unittest.TestCase):
     def test_read_data(self):
         expected_data = ['Aggie Pride Worldwide',
-                         'Aggies Do', 
+                         'Aggies Do',
                          'Go Aggies',
                          'Aggie Strong!',
                          'Go Aggies',
@@ -29,10 +29,7 @@ class DataTestCases(unittest.TestCase):
                          'Aggie Pride',
                          'Aggies are always number 1!',
                          'Because thats what Aggies do',
-                         'Aggie Bred',
-                         'Move forward with purpose',
-                         'GO Aggie!',
-                         'Aggie Pride']
+                         'Aggie Bred']
 
         data = read_data('sample_data.txt')
 
@@ -53,34 +50,120 @@ class DataTestCases(unittest.TestCase):
         self.assertFalse(test_data.has_us_phone())
 
     def test_has_email(self):
-        test_data = Pii()
-        self.assertEqual(test_data.has_email(), None)
+        test_data = Pii('My email is user@domain.com')
+        self.assertTrue(test_data.has_email())
+        test_data = Pii('My email is user@aggies.ncat.com')
+        self.assertTrue(test_data.has_email())
+        test_data = Pii('My email is user123@domain.com')
+        self.assertTrue(test_data.has_email())
+
+        test_data = Pii('My email is user123@')
+        self.assertFalse(test_data.has_email())
 
     def test_has_ipv4(self):
-        test_data = Pii()
-        self.assertEqual(test_data.has_ipv4(), None)
+        test_data = Pii('My ip address is 192.0.2.146')
+        self.assertTrue(test_data.has_ipv4())
+        test_data = Pii('My ip address is 1.7.33.132')
+        self.assertTrue(test_data.has_ipv4())
+
+        # bad symbol
+        test_data = Pii('My ip address is 1,7,33,132')
+        self.assertFalse(test_data.has_ipv4())
+        # bad length
+        test_data = Pii('My ip address is 1.7.33.132.0')
+        self.assertFalse(test_data.has_ipv4())
+        # Cant have a number above 256
+        test_data = Pii('My ip address is 1.7.33.256.0')
+        self.assertFalse(test_data.has_ipv4())
+        # Cant have a number below 0
+        test_data = Pii('My ip address is 1.7.33.-1.0')
+        self.assertFalse(test_data.has_ipv4())
 
     def test_has_ipv6(self):
-        test_data = Pii()
-        self.assertEqual(test_data.has_ipv6(), None)
+        test_data = Pii(
+            'My ip address is 2001:1db8:3333:4444:5555:6666:7777:8888')
+        self.assertTrue(test_data.has_ipv6())
+        test_data = Pii(
+            'My ip address is FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF')
+        self.assertTrue(test_data.has_ipv6())
+
+        # BAD LENGTH
+        test_data = Pii('My ip address is FFFF:FFFF:FFFF:FFFF:FFFF:FFFF')
+        self.assertFalse(test_data.has_ipv6())
+        # BAD SYMBOL
+        test_data = Pii(
+            'My ip address is 2001;1db8;3333;4444;5555;6666;7777;8888')
+        self.assertFalse(test_data.has_ipv6())
+        # Must be hexidecimal (0-9), (a-f), (A-F)
+        test_data = Pii(
+            'My ip address is 2001:1db8:3333:GGGG:5555:6666:7777:8888')
+        self.assertFalse(test_data.has_ipv6())
 
     def test_has_name(self):
-        test_data = Pii()
-        self.assertEqual(test_data.has_name(), None)
+        test_data = Pii('My name is John Smith')
+        self.assertTrue(test_data.has_name())
+        test_data = Pii('My name is Feng Tang')
+        self.assertTrue(test_data.has_name())
+        test_data = Pii('My name is Ty Rone')
+        self.assertTrue(test_data.has_name())
+
+        # First capital
+        test_data = Pii('My name is john Smith')
+        self.assertFalse(test_data.has_name())
+        # LAst capital
+        test_data = Pii('My name is John smith')
+        self.assertFalse(test_data.has_name())
+        # Conjoined
+        test_data = Pii('My name is JohnSmith')
+        self.assertFalse(test_data.has_name())
 
     def test_has_street_address(self):
-        test_data = Pii()
-        self.assertEqual(test_data.has_street_address(), None)
+        test_data = Pii('93 Garfield Street')
+        self.assertTrue(test_data.has_street_address())
+        test_data = Pii('636 Riverview Road')
+        self.assertTrue(test_data.has_street_address())
+        test_data = Pii('9104 Littleton Avenue')
+        self.assertTrue(test_data.has_street_address())
+
+        # TODO mabye valid idk
+        test_data = Pii('9104 Littleton Ave.')
+        self.assertFalse(test_data.has_street_address())
+        # Missing street#
+        test_data = Pii('Littleton Ave.')
+        self.assertFalse(test_data.has_street_address())
+        # Lowercase
+        test_data = Pii('9104 littleton')
+        self.assertFalse(test_data.has_street_address())
 
     def test_has_credit_card(self):
-        test_data = Pii()
-        self.assertEqual(test_data.has_credit_card(), None)
+        test_data = Pii('1929-1228-3455-3454')
+        self.assertTrue(test_data.has_credit_card())
+        test_data = Pii('2345-4567-5678-6789')
+        self.assertTrue(test_data.has_credit_card())
+        test_data = Pii('2345-1324-3456-1234')
+        self.assertTrue(test_data.has_credit_card())
+        test_data = Pii('5678-2349-7654-6435')
+        self.assertTrue(test_data.has_credit_card())
+
+        # bad symbol
+        test_data = Pii('3456=1234=5678=6789')
+        self.assertFalse(test_data.has_credit_card())
+        # missing symbol
+        test_data = Pii('1234764598764567')
+        self.assertFalse(test_data.has_credit_card())
 
     def test_has_at_handle(self):
-        test_data = Pii()
+        test_data = Pii('@tfeng')
+        self.assertEqual(test_data.has_at_handle(), None)
+        test_data = Pii('@github')
+        self.assertEqual(test_data.has_at_handle(), None)
+        test_data = Pii('@twitter123')
         self.assertEqual(test_data.has_at_handle(), None)
 
+        # TODO make bad inputs
+
     def test_has_pii(self):
+        # TODO make a long input
         test_data = Pii()
         self.assertEqual(test_data.has_pii(), None)
 
