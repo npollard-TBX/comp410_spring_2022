9ce9d9c2fe2b0078bba75ad91e9b829f83e7f232
Troy Boone <troy.boone@autodesk.com>
2022-02-21 12:30:37 -0500

    fixed autocomplete ipv4

--- test_pii_data.py
+++ test_pii_data.py
@@ -53,7 +53,7 @@ class DataTestCases(unittest.TestCase):
 
     def test_has_email(self):
         test_data = Pii()
-        self.assertEqual(test_data.has_ipv4(), None)
+        self.assertEqual(test_data.has_email(), None)
 
     def test_has_ipv4(self):
         test_data = Pii()
@@ -61,15 +61,15 @@ class DataTestCases(unittest.TestCase):
 
     def test_has_ipv6(self):
         test_data = Pii()
-        self.assertEqual(test_data.has_ipv4(), None)
+        self.assertEqual(test_data.has_ipv6(), None)
 
     def test_has_name(self):
         test_data = Pii()
-        self.assertEqual(test_data.has_ipv4(), None)
+        self.assertEqual(test_data.has_name(), None)
 
     def test_has_street_address(self):
         test_data = Pii()
-        self.assertEqual(test_data.has_ipv4(), None)
+        self.assertEqual(test_data.has_street_address(), None)
 
     def test_has_credit_card(self):
         test_data = Pii('1929-1228-3455-3454')
@@ -90,7 +90,7 @@ class DataTestCases(unittest.TestCase):
 
     def test_has_at_handle(self):
         test_data = Pii()
-        self.assertEqual(test_data.has_ipv4(), None)
+        self.assertEqual(test_data.has_at_handle(), None)
 
     def test_has_ssn(self):
         test_data = Pii('123-45-5667')
