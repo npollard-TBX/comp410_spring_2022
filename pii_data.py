719ac78ffe4e24cdfb3b4121b687f4c799aa1185
MNPSwingler <mnswingler@aggies.ncat.edu>
2022-03-01 23:23:56 -0500

    IPv4 and IPv6

--- pii_data.py
+++ pii_data.py
@@ -1,6 +1,7 @@
 import re
 
 
+
 # PII = Personally Identifiable Information
 # Create a new Pii class based on str
 class Pii(str):
@@ -9,35 +10,49 @@ class Pii(str):
     # https://www.w3schools.com/python/python_regex.asp
     def has_us_phone(self):
         # Match a US phone number ddd-ddd-dddd ie 123-456-7890
-        match = re.search(r'\d{3}-\d{3}-\d{4}', self)
+        return True if re.search(r'(\d{3}(-|.)\d{3}(-|.)\d{4})|\d{10}', self) else None
+
+    def has_email(self):
+        return True if re.search(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9]{2,}\b', self) else None
+
+    def has_ipv4(self):
+        #the 4 values in the IP address are from 0-255 for each segment each line is 1 segment
+        match = re.search(r'^\b([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b'
+        r'.\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b'
+        r'.\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b'
+        r'.\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b$', self)
+
         if match:
             return True
         return False
 
-    def has_email(self):
-        return None
-
-    def has_ipv4(self):
-        return None
-
     def has_ipv6(self):
-        return None
+        #There are 8 avaliable chunks to place IP data also allowing for no data to be input. Covers 0-9,a-f, and A-F
+        match = re.search(r'(^(\b[0-9A-Fa-f]{0,4}\b)?:(\b[0-9A-Fa-f]{0,4}\b)?:'
+        r'(\b[0-9A-Fa-f]{0,4}\b)?:(\b[0-9A-Fa-f]{0,4}\b)?:'
+        r'(\b[0-9A-Fa-f]{0,4}\b)?:(\b[0-9A-Fa-f]{0,4}\b)?:'
+        r'(\b[0-9A-Fa-f]{0,4}\b)?:(\b[0-9A-Fa-f]{0,4}\b)?$)', self)
+        if match:
+            return True
+        return False
 
     def has_name(self):
-        return None
+        return True if re.search(r'[A-Z][a-z]+\s[A-Z][a-z]+', self) else None
 
     def has_street_address(self):
-        return None
+        return True if re.search(r'[0-9]+\s[A-Z][a-z]+\s[A-Z][a-z]+', self) else None
 
     def has_credit_card(self):
-        return None
+        return True if re.search(r'(\d{4}-\d{4}-\d{4}-\d{4})|(\d{4}-\d{6}-\d{5})', self) else None
 
     def has_at_handle(self):
         return None
 
+    def has_ssn(self):
+        return True if re.search(r'\d{3}-\d{2}-\d{4}', self) else None
+
     def has_pii(self):
-        return self.has_us_phone() or self.has_email() or self.has_ipv4() or self.has_ipv6() or self.has_name() or \
-               self.has_street_address() or self.has_credit_card() or self.has_at_handle()
+        return self.has_us_phone() or self.has_email() or self.has_ipv4() or self.has_ipv6() or self.has_name() or self.has_street_address() or self.has_credit_card() or self.has_at_handle() or self.has_ssn()
 
 
 def read_data(filename: str):
--- test_pii_data.py
+++ test_pii_data.py
@@ -6,7 +6,7 @@ from pii_data import Pii
 class DataTestCases(unittest.TestCase):
     def test_read_data(self):
         expected_data = ['Aggie Pride Worldwide',
-                         'Aggies Do', 
+                         'Aggies Do',
                          'Go Aggies',
                          'Aggie Strong!',
                          'Go Aggies',
@@ -29,60 +29,146 @@ class DataTestCases(unittest.TestCase):
                          'Aggie Pride',
                          'Aggies are always number 1!',
                          'Because thats what Aggies do',
-                         'Aggie Bred',
-                         'Move forward with purpose',
-                         'GO Aggie!',
-                         'Aggie Pride']
+                         'Aggie Bred']
 
         data = read_data('sample_data.txt')
 
         self.assertEqual(data, expected_data)
+    def test_has_email(self):
+        test_data = Pii('My email is kaylahen2019@gmail.com')
+        self.assertEqual(test_data.has_email(), True)
+
+        test_data = Pii('My email is martin.complex@gmail.com')
+        self.assertEqual(test_data.has_email(), True)
+
+        test_data = Pii('My email is classof2023@aggies.ncat.edu')
+        self.assertEqual(test_data.has_email(), True)
+
+        test_data = Pii('My email is computerscience.com')
+        self.assertEqual(test_data.has_email(), None)
+
+        test_data = Pii('My email is engineering1gmail.com')
+        self.assertEqual(test_data.has_email(), None)
 
     def test_has_us_phone(self):
         # Test a valid US phone number
         test_data = Pii('My phone number is 970-555-1212')
+        self.assertTrue(test_data.has_us_phone())
+        # Test a valid US phone number
+        test_data = Pii('My phone number is 9705551212')
         self.assertTrue(test_data.has_us_phone())
 
         # Test a partial US phone number
         test_data = Pii('My number is 555-1212')
         self.assertFalse(test_data.has_us_phone())
 
-        # Test a phone number with incorrect delimiters
-        # TODO discuss changing requirements to support this
+        # Updated to allow for this entry
         test_data = Pii('My phone number is 970.555.1212')
-        self.assertFalse(test_data.has_us_phone())
+        self.assertTrue(test_data.has_us_phone())
 
     def test_has_email(self):
         test_data = Pii()
         self.assertEqual(test_data.has_email(), None)
 
     def test_has_ipv4(self):
-        test_data = Pii()
-        self.assertEqual(test_data.has_ipv4(), None)
+        #Successful test cases
+        test_data = Pii('222.33.100.12')
+        self.assertTrue(test_data.has_ipv4())
+
+        test_data = Pii('45.68.195.254')
+        self.assertTrue(test_data.has_ipv4())
+
+        # Test values are out of the range for allowed addresses
+        test_data = Pii('300.22.555.256')
+        self.assertFalse(test_data.has_ipv4())
+
+        # Test wrong format
+        test_data = Pii('145')  # incomplete address
+        self.assertFalse(test_data.has_ipv4())
+
 
     def test_has_ipv6(self):
-        test_data = Pii()
-        self.assertEqual(test_data.has_ipv6(), None)
+        # test a valid address
+        test_data = Pii('0045:Fa34:53d9:4d53:0020:0000:6491:8485')
+        self.assertTrue(test_data.has_ipv6())  
+
+        test_data = Pii('0000::5248:ee43::8789:1234:1200')
+        self.assertTrue(test_data.has_ipv6())  
+
+        # test an invalid address with to many digits in segment
+        self.assertFalse(test_data.has_ipv6())  
+        test_data = Pii('00000:::::')
+        
+        # test an invalid address w letter outside of bounds
+        self.assertFalse(test_data.has_ipv6())  
+        test_data = Pii('r445:rtyu:vd45:nmkl:af24:kb78')
 
     def test_has_name(self):
-        test_data = Pii()
+        test_data = Pii('My name is Alex Red')
+        self.assertEqual(test_data.has_name(), True)
+        test_data = Pii('William Dane')
+        self.assertEqual(test_data.has_name(), True)
+        test_data = Pii('kate')
+        self.assertEqual(test_data.has_name(), None)
+        test_data = Pii('rodger samson')
         self.assertEqual(test_data.has_name(), None)
 
     def test_has_street_address(self):
-        test_data = Pii()
+        test_data = Pii('My house is at 123 Chesnut Steet')
+        self.assertEqual(test_data.has_street_address(), True)
+        test_data = Pii('1235 Willow Way')
+        self.assertEqual(test_data.has_street_address(), True)
+        test_data = Pii('Raden Lane')
+        self.assertEqual(test_data.has_street_address(), None)
+        test_data = Pii('1345 chesnut steet')
         self.assertEqual(test_data.has_street_address(), None)
 
     def test_has_credit_card(self):
-        test_data = Pii()
-        self.assertEqual(test_data.has_credit_card(), None)
+        test_data = Pii('1929-1228-3455-3454')
+        self.assertTrue(test_data.has_credit_card())
+        test_data = Pii('2345-4567-5678-6789')
+        self.assertTrue(test_data.has_credit_card())
+        test_data = Pii('2345-1324-3456-1234')
+        self.assertTrue(test_data.has_credit_card())
+        test_data = Pii('5678-2349-7654-6435')
+        self.assertTrue(test_data.has_credit_card())
+
+        # Amex card case
+        test_data = Pii('1234-123456-12345')
+        self.assertTrue(test_data.has_credit_card())
+
+        # bad symbol
+        test_data = Pii('3456=1234=5678=6789')
+        self.assertFalse(test_data.has_credit_card())
+        # missing symbol
+        test_data = Pii('1234764598764567')
+        self.assertFalse(test_data.has_credit_card())
 
     def test_has_at_handle(self):
         test_data = Pii()
         self.assertEqual(test_data.has_at_handle(), None)
 
+    def test_has_ssn(self):
+        test_data = Pii('123-45-5667')
+        self.assertTrue(test_data.has_ssn())
+        test_data = Pii('654-45-3456')
+        self.assertTrue(test_data.has_ssn())
+        test_data = Pii('098-67-9878')
+        self.assertTrue(test_data.has_ssn())
+
+        test_data = Pii('098.67.9878')
+        self.assertFalse(test_data.has_ssn())
+        test_data = Pii('098679878')
+        self.assertFalse(test_data.has_ssn())
+        test_data = Pii('098-6-9878')
+        self.assertFalse(test_data.has_ssn())
+
+        # TODO make bad inputs
+
     def test_has_pii(self):
         test_data = Pii()
         self.assertEqual(test_data.has_pii(), None)
+
 
 
 if __name__ == '__main__':
