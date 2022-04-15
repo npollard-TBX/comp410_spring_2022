ad6795963c9a438300df1f958c262379c9be731c
npollard-TBX <54555322+npollard-TBX@users.noreply.github.com>
2022-04-14 23:45:26 -0400

    Add files via upload

---
+++ comp410_spring_2022/pii_data.py
@@ -0,0 +1,116 @@
+import re
+
+
+# PII = Personally Identifiable Information
+# Create a new Pii class based on str
+class Pii(str):
+    # For help with regex see
+    # https://regex101.com
+    # https://www.w3schools.com/python/python_regex.asp
+    def has_us_phone(self, anonymize= false):
+        # Match a US phone number ddd-ddd-dddd ie 123-456-7890
+        match = re.search(r'\d{3}-\d{3}-\d{4}', '[us phone]', self)
+
+        if anonymize:
+            return match
+        else:
+            return True if match != self else None
+
+    def has_email(self, anonymize= False):
+        match = re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.]{2,}\b','[email address]', self)
+        
+        if anonymize:
+            return match
+        else:
+            return True if match != self else None
+
+    def has_ipv4(self, anoymize= False):
+        # Match all forms of IPv4
+        match = re.sub("(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[1-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])","[IPv4 address]", self)
+
+        if anoymize:
+            return match
+        else:
+            return True if match != self else None
+
+    def has_ipv6(self, anonymize=False):
+        # Match all forms of IPv6
+        match = re.sub("(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))", "[IPv6 address]",self)
+
+        if anonymize:
+            return match
+        else:
+            return True if match != self else None
+
+    def has_name(self, anonymize=False):
+        match = re.sub(r'\b([A-Z]{1}[a-z]+\s{1})([A-Z]{1}[a-z]+)\b','[name]', self)
+        if anonymize:
+            return match
+        else:
+            return True if match != self else None
+
+
+    def has_street_address(self, anonymize=False):
+        match = re.sub(r'\d{0,9}\s{1}\b([A-Z]{1}[a-z]+\s{1})([A-Z]{1}[a-z]+)\b','[street address]', self)
+        if anonymize:
+            return match
+        else:
+            return True if match != self else None
+
+
+
+    def has_credit_card(self,anonymize=False):
+        match = re.sub(r'\d{4}-\d{4}-\d{4}-\d{4}','[credit card]', self)
+        if anonymize:
+            return match
+        else:
+            return True if match!= self else None
+
+    def has_at_handle(self,anonymize=False):
+        
+        #Match @ handles for twitter
+        match = re.sub(r'[\@][A-z0-9][A-z0-9.]{0,15}','[at handle]', self)
+        if anonymize:
+            return match
+        else:
+            print (match)
+            return True if match!= self else None
+            
+
+    
+    def has_ssn(self, anonymize= False):
+        match = re.sub(r'\d{3}-\d{2}-\d{4}','[ssn number]', self)
+        
+        if anonymize:
+            return match
+        else:
+            return True if match != self else None
+
+
+    def has_pii(self):
+        return self.has_us_phone() or self.has_email() or self.has_ipv4() or self.has_ipv6() or self.has_name() or self.has_street_address() or self.has_credit_card() or self.has_at_handle() or self.has_ssn()
+
+
+def read_data(filename: str):
+    data = []
+    with open(filename) as f:
+        # Read one line from the file stripping off the \n
+        for line in f:
+            data.append(line.rstrip())
+    return data
+
+
+if __name__ == '__main__':
+
+    # Removed hard coded windows path.  We'll discuss this in sprint3
+    # data = read_data('C:\Users\taido\comp4100\comp410_spring_2022\sample_data.txt')
+    # print(data)
+    # print('---')
+
+    pii_data = Pii('My phone number is 123-123-1234')
+    print(pii_data)
+
+    if pii_data.has_pii():
+        print('There is PII data preset')
+    else:
+        print('No PII data detected')
---
+++ comp410_spring_2022/test_pii_data.py
@@ -0,0 +1,169 @@
+import unittest
+from pii_data import read_data
+from pii_data import Pii
+
+
+class DataTestCases(unittest.TestCase):
+    def test_read_data(self):
+        expected_data = ['Aggie Pride Worldwide',
+                         'Aggies Do', 
+                         'Go Aggies',
+                         'Aggie Strong!',
+                         'Go Aggies',
+                         'And Thats on 1891',
+                         "Let's Go Aggies",
+                         'Never Ever Underestimate an Aggie',
+                         'Every Day The Aggie Way',
+                         'Can I get an Aggie Pride',
+                         'Aggies Do ^2',
+                         'Aggie Pride For The Culture',
+                         'We Are Aggies! We Are Proud!',
+                         'Set My Future Self Up for Success!',
+                         'AGGIE PRIDE!',
+                         'We are Aggies',
+                         'A-G-G-I-E, WHAT? P-R-I-D-E',
+                         'Aggie Pride',
+                         'Leaders Can Aggies Do',
+                         'Mens et Manus',
+                         'Aggies Aggies Aggies',
+                         'Aggie Pride',
+                         'Aggies are always number 1!',
+                         'Because thats what Aggies do',
+                         'Aggie Bred',
+                         'Move forward with purpose',
+                         'GO Aggie!',
+                         'Aggie Pride']
+
+        data = read_data('sample_data.txt')
+
+        self.assertEqual(data, expected_data)
+
+    def test_has_us_phone(self):
+        # Test a valid US phone number
+        test_data = Pii('My phone number is 970-555-1212')
+        self.assertTrue(test_data.has_us_phone())
+
+        test_data = Pii('My phone number is 8048226077')
+        self.assertTrue(test_data.has_us_phone())
+
+        # Test a partial US phone number
+        test_data = Pii('My phone number is 555-1212')
+        self.assertFalse(test_data.has_us_phone())
+
+        # Test a phone number with incorrect delimiters
+        # TODO discuss changing requirements to support this
+        test_data = Pii('My phone number is 970.555.1212')
+        self.assertFalse(test_data.has_us_phone())
+
+    def test_has_us_phone_anonymize(self):
+        test_data = Pii('My phone number is 664-245-2467')
+        self.assertEqual(test_data.has_us_phone(anonymize=True), 'My phone number is [us phone]')
+
+    def test_has_email(self):
+        test_data = Pii('My email is user@doamin.com')
+        self.assertTrue(test_data.has_email())
+        
+    def test_has_email_anonymize(self):
+        test_data = Pii('My email is johndoe@domain.com')
+        self.assertEqual(test_data.has_email(anonymize=True), 'My email is [email address]')
+
+    def test_has_ipv4(self):
+        test_data = Pii('176.96.81.20')
+        self.assertTrue(test_data.has_ipv4())
+
+    def test_has_ipv4_anonymize(self):
+        test_data = Pii('This... 176.96.81.20 is my IP')
+        self.assertEqual(test_data.has_ipv4(anoymize=True), 'This... [IPv4 address] is my IP')
+
+    def test_has_ipv6(self):
+        test_data = Pii('2001:0db8:85a3:0000:0000:8a2e:0370:7334')
+        self.assertTrue(test_data.has_ipv6())
+
+    def test_has_ipv6_anonymize(self):
+        test_data = Pii('My IP address is 2001:0db8:85a3:0000:0000:8a2e:0370:7334')
+        self.assertEqual(test_data.has_ipv6(anonymize=True),"My IP address is [IPv6 address]")
+
+    def test_has_name(self):
+        test_data = Pii('My name is John Smith')
+        self.assertTrue(test_data.has_name())
+
+        test_data = Pii('My name is john smith')
+        self.assertFalse(test_data.has_name())
+
+    def test_has_name_anonymize(self):
+        self.assertEqual(Pii('My name is John Smith').has_name(anonymize=True),
+                         'My name is [name]')
+        #try an invalid name
+        self.assertEqual(Pii('My name is john smith').has_name(anonymize=True),
+                         'My name is john smith')
+
+    def test_has_street_address(self):
+        test_data = Pii('My address is 123 Chestnut Lane')
+        self.assertTrue(test_data.has_street_address())
+
+        test_data = Pii('My address is 123 chestnut lane')
+        self.assertFalse(test_data.has_street_address())
+
+    def test_has_street_address_anonymize(self):
+        self.assertEqual(Pii('My address is 123 Chestnut Lane').has_street_address(anonymize=True),
+                         'My address is [street address]')
+        #try an invalid street address
+        self.assertEqual(Pii('My address is 123 chestnut lane').has_street_address(anonymize=True),
+                         'My address is 123 chestnut lane')
+
+    def test_has_credit_card(self):
+        #Test case for valid credit card
+        test_data = Pii('My card is 1234-1234-1234-1234')
+        self.assertTrue(test_data.has_credit_card())
+        # Test case for an invalid card
+        test_data = Pii('My card is 123456-123456-1234-1234')
+        self.assertFalse(test_data.has_credit_card())
+        test_data = Pii('My card is 123a-1256-14-124')
+        self.assertFalse(test_data.has_credit_card())
+
+    def test_has_credit_card_anonymize(self):
+        test_data = Pii('My card is 1234-1234-1234-1234')
+        self.assertEqual(test_data.has_credit_card(anonymize=True),'My card is [credit card]')
+
+
+    def test_has_at_handle(self):
+
+       #Test case for valid @ handle
+        test_data = Pii('My social media handle is @sushi_fein')
+        self.assertTrue(test_data.has_at_handle())
+
+
+        #Test case for invalid  @ handle
+        test_data = Pii('My social media handle is @.sushifein')
+        self.assertFalse(test_data.has_at_handle())
+
+
+    def test_has_at_handle_anonymize(self):
+
+       #Test case for anonymizing @ handle
+        test_data = Pii('My social media handle is @sushi_fein')
+        self.assertEqual(test_data.has_at_handle(anonymize=True),'My social media handle is [at handle]')
+
+
+        #Test case for anonymize invalid @ handle
+        test_data = Pii('My social media handle is @sushi_fein on twitter but on snapchat it is @sushi.fein')
+        self.assertEqual(test_data.has_at_handle(anonymize=True),'My social media handle is [at handle] on twitter but on snapchat it is [at handle]')
+
+
+            
+    def test_has_ssn(self):
+        test_data = Pii('123-45-6789')
+        self.assertTrue(test_data.has_ssn())
+        
+    def test_has_ssn_anonymize(self):
+        test_data = Pii('My ssn is 123-45-6789')
+        self.assertEqual(test_data.has_ssn(anonymize=True), 'My ssn is [ssn number]')
+
+
+    def test_has_pii(self):
+        test_data = Pii()
+        self.assertEqual(test_data.has_pii(),None)
+
+
+if __name__ == '__main__':
+    unittest.main()
